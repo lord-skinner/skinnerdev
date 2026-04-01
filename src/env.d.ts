4b3554466ea88a7a79688b3e9dc28621345dbf8f
lord-skinner <mshskinner@gmail.com>
2026-04-01 12:34:51 -0500

    pushing changes

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,4 +5,6 @@ interface ImportMetaEnv {
 	readonly PUBLIC_GISCUS_REPO_ID?: string;
 	readonly PUBLIC_GISCUS_CATEGORY?: string;
 	readonly PUBLIC_GISCUS_CATEGORY_ID?: string;
+	/** Google Analytics 4 measurement ID (G-XXXXXXXX). Enables GA4 when set. */
+	readonly PUBLIC_GA_MEASUREMENT_ID?: string;
 }
